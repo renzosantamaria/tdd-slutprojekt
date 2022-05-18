d18c72382d847bc4b752e1e6a2ed77e378b4c9b8
renzosantamaria <renzo.santamaria@outlook.com>
2022-05-18 18:03:04 +0200

    render list of meetups

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,48 @@
-import React, { FC } from "react"
+import { FC, useEffect } from "react"
+import Meetup from "../../components/UI/Meetup/Meetup"
+import { getAllUpcomingMeetups, getAllPastMeetups } from '../../API'
+import { getCurrentMeetups, getPastMeetups } from "../../store/meetup/meetupSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from "../../store/store"
 
 const Home: FC = () => {
+    const dispatch = useDispatch()
+    const currentMeetups = useSelector((state: RootState) => state.meetups.currentMeetups)
+    const pastMeetups = useSelector((state: RootState) => state.meetups.pastMeetups)
+
+    useEffect(() => {
+        const currentMeetups = getAllUpcomingMeetups(new Date())
+        dispatch(getCurrentMeetups(currentMeetups))
+        const pastMeetups = getAllPastMeetups(new Date())
+        dispatch(getPastMeetups(pastMeetups))
+    }, [dispatch])
+
     return (
         <main>
-            <h1>Welcome to Home</h1>
+            <section>
+                <h2>Upcoming meetups</h2>
+                {currentMeetups.length > 0 ? 
+                    <ul>
+                        {currentMeetups!.map(meetup => (
+                            <Meetup meetup={meetup} testId={'currentListItem'} key={meetup.id} />
+                        ))}
+                    </ul>
+                    :
+                    <p>No upcoming meetups to show</p>
+                }
+            </section>
+            <section>
+                <h2>Past meetups</h2>
+                {pastMeetups.length > 0 ? 
+                    <ul>
+                        {pastMeetups!.map(meetup => (
+                            <Meetup meetup={meetup} testId={'currentListItem'} key={meetup.id} />
+                        ))}
+                    </ul>
+                    :
+                    <p>No past meetups to show</p>
+                }
+            </section>
         </main>
     )
 }
