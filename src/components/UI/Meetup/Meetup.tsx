5c9b91a52538243887dfa96aed5e69546200ad0c
renzosantamaria <renzo.santamaria@outlook.com>
2022-05-18 18:01:37 +0200

    add meetup component

---
+++ src/components/UI/Meetup/Meetup.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react"
+import { IMeetup } from '../../../store/meetup/meetup.types'
+import { Link } from 'react-router-dom'
+
+interface Props {
+  meetup: IMeetup
+  testId: string
+}
+
+const Meetup: FC<Props> = (props) => {
+    const { meetup, testId } = props
+    return (
+        <Link to={`/meetups/${meetup.id}`} >
+      <div role="listitem" data-testid={testId} >
+        <h3>
+          {meetup.title}
+        </h3>
+
+        <p>
+          <strong>Start: </strong>
+          {meetup.startDate.toDateString()} {meetup.startDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
+        </p>
+        <p></p>
+        <p>
+          <strong>End: </strong>
+          {meetup.endDate.toDateString()} {meetup.endDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
+        </p>
+        <p>
+          <strong>Address: </strong>
+          {meetup.address}
+        </p>
+      </div>
+    </Link>
+    )
+}
+
+export default Meetup
