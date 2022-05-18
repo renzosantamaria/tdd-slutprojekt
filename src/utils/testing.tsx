3e795b495d24a1690ea10524e551c16cd78e6f21
renzosantamaria <renzo.santamaria@outlook.com>
2022-05-18 14:51:45 +0200

    add testing util

---
+++ src/utils/testing.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react'
+import { ReactElement } from 'react'
+import { MemoryRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router'
+import { makeStore } from '../store/store'
+import { Provider } from 'react-redux'
+
+const store = makeStore()
+const renderWithRouter = (ui: ReactElement, options?: any) => render(ui, { wrapper: MemoryRouter, ...options })
+const renderWithPath = (entry: string, ui: ReactElement, path: string) =>
+
+  render(
+    <MemoryRouter initialEntries={[entry]}>
+      <Provider store={store}>
+        <Routes>
+          <Route path={path} element={ui} />
+        </Routes>
+      </Provider>
+    </MemoryRouter>
+  )
+export { renderWithRouter, renderWithPath }
