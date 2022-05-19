1b2e952d50dd33ceb65f179add5dc59376dcbb88
renzosantamaria <renzo.santamaria@outlook.com>
2022-05-19 12:30:50 +0200

    add integrations tests

--- src/pages/Home/Home.unit.test.tsx
+++ src/pages/Home/Home.unit.test.tsx
@@ -1,3 +1,4 @@
+import { screen } from '@testing-library/react';
 import { renderWithRouter } from '../../utils/testing'
 import Home from './Home'
 import { makeStore } from '../../store/store'
@@ -12,6 +13,22 @@ beforeEach(() => {
   )
 })
 
-describe('Home unit tests - meetup lists', () => {
+describe('Home integration tests - meetup lists', () => {
   it('renders Home component correctly (smoketest)', () => {})
+  it('renders a heading element for the upcoming evets list', () => {
+    const upcomingMeetupsHeading = screen.getByRole('heading', {name: /Upcoming meetups/i})
+    expect(upcomingMeetupsHeading).toBeInTheDocument()
+  })
+  it('renders a heading element for the past evets list', () => {
+    const pastMeetupsHeading = screen.getByRole('heading', {name: /Past meetups/i})
+    expect(pastMeetupsHeading).toBeInTheDocument()
+  })
+  it('renders a list of 2 coming meetups', async () => {
+    const meetups = await screen.findAllByTestId('upcomingMeetup')
+    expect(meetups).toHaveLength(2)
+  })
+  it('renders a list of 2 past meetups', async () => {
+    const meetups = await screen.findAllByTestId('pastMeetup')
+    expect(meetups).toHaveLength(2)
+  })
 })
