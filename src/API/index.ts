1b21ca4eb05c19509a3ba14a6f99e2063a5b09e7
renzosantamaria <renzo.santamaria@outlook.com>
2022-05-19 20:11:47 +0200

    add login api

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,20 +1,52 @@
 import {meetups} from '../data'
 import {users} from '../data'
+import { IUser } from '../store/user/user.types'
 
 export const getAllUpcomingMeetups = (currentDate: Date) => {
     let now = currentDate.getTime()
   
     const comingMeetups = meetups.filter(meetup => meetup.startDate.getTime() > now)
     return comingMeetups.slice().sort((a, b) => +a.startDate - +b.startDate)
-  }
-  
+}
 export const getAllPastMeetups = (currentDate: Date) => {
   let now = currentDate.getTime()
 
   const pastMeetups = meetups.filter(meetup => meetup.startDate.getTime() < now)
   return pastMeetups.slice().sort((a, b) => +b.startDate - +a.startDate)
 }
-
 export const getMeetupById = (id: number) => {
   return meetups.find(meetup => meetup.id === id)
+}
+export const authenticateUser = (email: string, password: string): IUser | undefined => {
+  const user = users.find(user => user.email === email)
+
+  return user && user.password === password
+    ? { id: user.id, name: user.name, email: user.email, attendingMeetupsIds: user.attendingMeetupsIds, hostingMeetupsIds: user.hostingMeetupsIds }
+    : undefined
+}
+export const updateMeetupAttendeeList = (id: number, attendeeName: string) => {
+  let meetupIndex = meetups.findIndex(meetup => meetup.id === id)
+  let meetup = meetups.find(meetup => meetup.id === id)
+
+  if (meetup) {
+    let newMeetup = { ...meetup }
+    newMeetup.attendees = [...newMeetup.attendees, attendeeName]
+    meetups[meetupIndex] = newMeetup
+    return newMeetup
+  } else {
+    return undefined
+  }
+}
+export const updateUserAttendingMeetupsIds = (userId: number, meetupId: number) => {
+  const userIndex = users.findIndex(user => user.id === userId)
+  const user = users.find(user => user.id === userId)
+
+  if (user) {
+    const updatedUser = { ...user }
+    updatedUser.attendingMeetupsIds = [...updatedUser.attendingMeetupsIds, meetupId]
+    users[userIndex] = updatedUser
+    return updatedUser
+  } else {
+    return undefined
+  }
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,51 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { login } from '../../store/user/userSlice'
+import { authenticateUser } from "../../API"
 
 const Login: FC = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [authError, setAuthError] = useState<boolean>(false)
+
+    const handleLogin = (e: React.FormEvent) => {
+        e.preventDefault()
+        setAuthError(false)
+        const user = authenticateUser(email, password)
+        if (user) {
+          dispatch(login(user))
+          navigate('/')
+        } else {
+          setAuthError(true)
+        }
+    }
+
     return (
         <main>
-            <h1>Welcome to Login</h1>
+            <h2>Log in</h2>
+            <form name="login-form" onSubmit={handleLogin}>
+                <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    placeholder="user1@email.com"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                </div>
+                <div>
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+                </div>
+                {authError && <p>This user does not exist, check your spelling.</p>}
+                <button data-testid="login-btn">Log in</button>
+            </form>
         </main>
     )
 }
