84753247e5a5dd10b744f9f213a9549c29d49413
renzosantamaria <renzo.santamaria@outlook.com>
2022-05-19 16:10:24 +0200

    update variable name

--- src/pages/MeetupDetails/MeetupDetails.tsx
+++ src/pages/MeetupDetails/MeetupDetails.tsx
@@ -10,14 +10,14 @@ interface CustomLocationState {
 const MeetupDetails: FC = () => {
     const {id} = useParams()
     const [meetup, setMeetup] = useState<IMeetup>()
-    const [meetupNoExist, setMeetupNoExists] = useState<boolean>(true)
+    const [meetupExist, setMeetupExists] = useState<boolean>(false)
     const location = useLocation()
     const state = location.state as CustomLocationState
     const past = state&&state.pastMeetup || undefined
 
     useEffect(() => {
         const currentMeetup = getMeetupById(+id!)
-        currentMeetup ? setMeetup(currentMeetup) : setMeetupNoExists(false)
+        currentMeetup ? setMeetup(currentMeetup) : setMeetupExists(false)
         
     }, [])
 
@@ -37,18 +37,18 @@ const MeetupDetails: FC = () => {
                     <p>
                         Ends:
                         {meetup.endDate.toDateString()}{' '}
-                        {meetup.startDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
+                        {meetup.endDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
                     </p>
                     <p>Address: {meetup.address}</p>
                     {meetup.attendees.length> 0 ?
-                    <p>Attendees: {meetup.attendees.join(', ')}</p>
+                    <p>Attendees: {meetup.attendees.join(',  ')}.</p>
                     :   
                     ''
                     }
                 </div>
             </>
             :
-            <p>{meetupNoExist.toString()}</p>
+            !meetupExist && <p>Nothing to see here</p>
             }
         </main>
     )
