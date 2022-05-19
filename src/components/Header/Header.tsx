5984c4d2eea4b06dc85f29ed87be895fcc2670d4
renzosantamaria <renzo.santamaria@outlook.com>
2022-05-19 20:13:00 +0200

    add login

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,29 @@
 import { FC } from "react"
 import { useSelector, useDispatch } from "react-redux"
-import { useNavigate, Link } from "react-router-dom"
-import { classicNameResolver } from "typescript"
+import { useNavigate, Link, useMatch } from "react-router-dom"
+import { RootState } from "../../store/store"
+import { logout } from "../../store/user/userSlice"
 import classes from './Header.module.css'
 
 const Header: FC = () => {
+    const user = useSelector((state: RootState) => state.user.user)
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const insideLoginPage = useMatch('login')
+
+    const handleLogout = () => {
+        dispatch(logout())
+        navigate('/')
+    }
     return (
         <header className={classes.header}>
             <nav className={classes.navigation}>
                 <Link to="/">
                     <h1>Home</h1>
                 </Link>
-                <Link to="/login">
-                    <h1>Login</h1>
-                </Link>
+                {!user && !insideLoginPage && <Link to="/login">Login</Link>}
+                {user && <button onClick={handleLogout}>Logout</button>}
             </nav>
         </header>
     )
